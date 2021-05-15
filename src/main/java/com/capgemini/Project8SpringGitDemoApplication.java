089c16780e68b78fe3009ffd6ae13d51f5923527
Santosh Mondal <santosh.ece06@gmail.com>
2021-05-15 09:47:02 +0530

    v1

---
+++ src/main/java/com/capgemini/Project8SpringGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project8SpringGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project8SpringGitDemoApplication.class, args);
+	}
+
+}
